4758392eb2701aa3b88a0c056fe3a415851876ef
Szuhay Dávid <szuhaydv@gmail.com>
2023-12-04 15:21:47 +0100

    v2.20

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 const corsAllowed = {
-    origin: 'https://todo-frontend-q9k5.onrender.com',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowHeaders: ['Content-Type'],
     credentials: true 
