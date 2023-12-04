56c6a1145146b248510750d30f37c3ddef480702
Szuhay Dávid <szuhaydv@gmail.com>
2023-12-04 18:38:33 +0100

    v3.011

--- routes/authMiddleware.js
+++ routes/authMiddleware.js
@@ -1,6 +1,8 @@
 const isAuth = (req, res, next) => {
-  if (req.isAuthenticated()) {
-      next();
+  if (req.user) {
+      res.status(123).send(req.user)
+  } else if (req.session.passport.user) {
+      res.status(234).send(req.session.passport.user)
   } else {
       res.status(401).json({ msg: 'You are fkn stupid or something' });
   }
