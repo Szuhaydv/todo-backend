7d4212e11287178e6ab2e6d71aae4d44ceab30e2
Szuhay Dávid <szuhaydv@gmail.com>
2023-12-04 19:17:02 +0100

    v3.16

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }), (req,res,next) => {
-  return res.status(200).send({message: "Successful login!"})
+  return res.status(200).send({message: req.user})
 })
 
 app.post('/register', async (req, res, next) => {
