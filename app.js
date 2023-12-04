c391ea8d6e46a4d3ccc0e9475c1187636b8a88d1
Szuhay Dávid <szuhaydv@gmail.com>
2023-12-04 15:51:03 +0100

    cookie-samesite

--- app.js
+++ app.js
@@ -48,7 +48,8 @@ app.use(session({
     cookie: {
         maxAge: 1000 * 60 * 60 * 24,
         httpOnly: false,
-        sameSite: "none"
+        sameSite: "none",
+        secure: true
     }
 }));
 
