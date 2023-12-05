9a9a68dc7e96a7689eb827ea2a5b9c424604b543
Szuhay Dávid <szuhaydv@gmail.com>
2023-12-05 18:11:02 +0100

    v4.21

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(session({
         maxAge: 1000 * 60 * 60 * 24,
         httpOnly: true,
         sameSite: "none",
-        secure: true,
+        secure: false,
         domain: 'todo-backend1-0rrs.onrender.com'
     }
 }));
