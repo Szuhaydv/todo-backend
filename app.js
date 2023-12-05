d32829fdb5de09b7f36aeef466f0c110d0155d5a
Szuhay Dávid <szuhaydv@gmail.com>
2023-12-05 15:01:46 +0100

    v4.2

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 const corsAllowed = {
-    origin: ['*'],
+    origin: ['https://mylittletodo.site/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowHeaders: ['*'],
     credentials: true 
